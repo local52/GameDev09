f63be36c58e4dd81d41fbe0bb78c8d1d708a6ce7
tomoyamiyoshikun <tomoyamiyoshikun@gmail.com>
2025-09-18 20:12:55 +0900

    [Change]

--- Assets/Scripts/ScoreBasedSceneLoader3.cs
+++ Assets/Scripts/ScoreBasedSceneLoader3.cs
@@ -1,18 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScoreBasedSceneLoader3 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("スコアの閾値を設定")]
+    [SerializeField] private float lowScoreThreshold = 30f;   // この値未満なら Low シーン
+    [SerializeField] private float highScoreThreshold = 70f;  // この値以上なら High シーン
+
+    [Header("スコアが閾値未満のときのシーン名")]
+    [SerializeField] private string lowScoreScene;
+
+    [Header("スコアが閾値の間のときのシーン名")]
+    [SerializeField] private string midScoreScene;
+
+    [Header("スコアが高スコア閾値以上のときのシーン名")]
+    [SerializeField] private string highScoreScene;
+
+    // UIボタンから呼び出すメソッド
+    public void LoadSceneBasedOnScore()
     {
-        
+        float currentScore = ScoreManeg.Score;
+        Debug.Log("▶ 現在スコア: " + currentScore);
+
+        if (currentScore < lowScoreThreshold)
+        {
+            LoadScene(lowScoreScene, "Low");
+        }
+        else if (currentScore < highScoreThreshold)
+        {
+            LoadScene(midScoreScene, "Mid");
+        }
+        else
+        {
+            LoadScene(highScoreScene, "High");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LoadScene(string sceneName, string label)
     {
-        
+        if (!string.IsNullOrEmpty(sceneName))
+        {
+            Debug.Log($"▶ {label} スコア: シーン遷移 -> {sceneName}");
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            Debug.LogWarning($"⚠ {label} シーン名が設定されていません！");
+        }
     }
 }
